ad0d50c38336970000896a09571980dd41979857
Rob Trew <houthakker72@gmail.com>
2012-12-10 22:11:49 +0000

    Added script for logging OmniFocus done tasks to DayOne without duplication
    Maintains a text file of logged items to avoid duplication.

---
+++ OmniFocus scripts/Shell scripts for Geektool or logging/2012-12-10-omnifocus-done-to-DayOne-shellscript.md
@@ -0,0 +1,33 @@
+---
+layout: default
+title: Log OmniFocus items done today to DayOne
+description: Logs today's OmniFocus done items to DayOne (avoiding duplication)
+---
+
+{{page.description}}
+
+Shell script which logs, to the [DayOne application](http://dayoneapp.com), any OmniFocus tasks which :
+
+1.	Are marked as completed since midnight last night, and
+2.	have not yet been logged to DayOne.
+
+- The script directly queries the OmniFocus Sqlite cache, and doesn't require OmniFocus to be running.
+- It avoids duplicated logging of items logged earlier, if the script is called several times in one day.
+- (Should, in short, be usable at regular intervals from Hazel, if that suits your workflow)
+
+The default output format is broadly that of TaskPaper or FoldingText:
+
+    DONE TODAY (6)
+
+    Project A:
+    - task1 @done(yyyy-mm-dd HH:MM)
+    - task2 @done(yyyy-mm-dd HH:MM)
+    - taskN @done(yyyy-mm-dd HH:MM)
+
+    Project B:
+    - taskX @done(yyyy-mm-dd HH:MM)
+    - taskY @done(yyyy-mm-dd HH:MM)
+    - taskZ @done(yyyy-mm-dd HH:MM)
+
+
+- [**View Script**](https://github.com/RobTrew/tree-tools/blob/master/OmniFocus%20scripts/Shell%20scripts%20for%20Geektool%20or%20logging/OmniFocusLogDone2DayOne.sh)
---
+++ OmniFocus scripts/Shell scripts for Geektool or logging/OmniFocusLogDone2DayOne.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Ver 0.02
+# Logs today's OmniFocus DONE items in DAYONE, 
+# Avoiding duplication if called several times in one day
+# ( Maintains a text file list of which items have already been logged today )
+# Intended for Hazel use.
+
+DONE_LOG_FOLDER="$HOME"
+DONE_TODAY_FILE="$DONE_LOG_FOLDER/DoneToday.txt"
+LOGGED_TODAY_FILE="$DONE_LOG_FOLDER/LoggedToday.txt"
+LOG_NOW_FILE="$DONE_LOG_FOLDER/JustDone.txt"
+
+OFOC="com.omnigroup.OmniFocus"
+if [ ! -d "$HOME/Library/Caches/$OFOC" ]; then OFOC=$OFOC.MacAppStore; fi
+OFQUERY="sqlite3 $HOME/Library/Caches/$OFOC/OmniFocusDatabase2"
+START_OF_DAY=$(date -v0H -v0M -v0S +%s) #Midnight at the start of today: set the time component to 00:00
+TODAY=$(date "+%Y-%m-%d")
+
+# Suggestions welcome here - not sure this simple arithmetic works for New Zealand's DST +1300 UTC
+ZONERESET=$(date +%z | awk '
+{if (substr($1,1,1)!="+") {printf "+"} else {printf "-"} print substr($1,2,4)}') 
+YEARZERO=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "2001-01-01 0:0:0 $ZONERESET" "+%s")
+DONE="($YEARZERO + t.dateCompleted)";
+
+# This join includes context & folder, to allow for broader queries
+JOIN="(((task tt left join projectinfo pi on tt.containingprojectinfo=pi.pk) t
+left join task p on t.task=p.persistentIdentifier)
+left join context c on t.context = c.persistentIdentifier)
+left join folder f on t.folder=f.persistentIdentifier"
+
+MATCHES="$DONE > $START_OF_DAY"
+# doneTOTAL=$($OFQUERY "SELECT count(*) FROM $JOIN WHERE $MATCHES;")
+# printf "DONE TODAY (%s)\n\n" "$doneTOTAL"
+
+$OFQUERY "
+SELECT t.persistentIdentifier, strftime('%Y-%m-%d|%H:%M',$DONE, 'unixepoch'), p.name, t.name
+FROM $JOIN WHERE $MATCHES ORDER BY t.datecompleted 
+" > $DONE_TODAY_FILE
+# Determine what, if anything needs to be logged
+if [ -f $LOGGED_TODAY_FILE ]; then
+    if grep -q $TODAY $LOGGED_TODAY_FILE; then
+       awk 'FNR==NR{old[$0];next};!($0 in old)' $LOGGED_TODAY_FILE $DONE_TODAY_FILE > $LOG_NOW_FILE
+    else
+        cp -f $DONE_TODAY_FILE $LOG_NOW_FILE
+    fi
+else # Log the whole file, then copy done file to logged file
+   cp -f $DONE_TODAY_FILE $LOG_NOW_FILE
+fi
+# then if the log_now file contains entries log them, and append the list to the logged today
+if [ -s $LOG_NOW_FILE ] ; then
+echo -n "" > "tmp_pretty.txt"
+cat $LOG_NOW_FILE | awk '
+BEGIN {FS="\|"; prj=0; str=""}
+{
+  if (prj!=$4) {prj=$4;
+      if (prj!="") {print ("\n## " prj ":") >> "tmp_pretty.txt" } 
+  }
+  if ($5!=prj) {print ("- " $5 " @done(" $3 ")") >> "tmp_pretty.txt"  }
+  else {{print "- *Project completed* @done(" $3 ")"  >> "tmp_pretty.txt" }}
+}'
+cat tmp_pretty.txt | /usr/local/bin/dayone new
+rm tmp_pretty.txt
+cat $LOG_NOW_FILE >> $LOGGED_TODAY_FILE  # Append the list of logged tasks to avoid duplication
+fi
+
+# For more details of the dayone command line tool, try:
+# man -t 'dayone' | pstopdf -i -o ~/Desktop/dayone.pdf
