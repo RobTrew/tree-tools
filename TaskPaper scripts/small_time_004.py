7da596985ea72adc55c2110ac71eb71cd31a4f6a
Rob Trew <houthakker72@gmail.com>
2014-02-14 17:04:59 +0000

    updated small_time_oo4.py

--- TaskPaper scripts/small_time_004.py
+++ TaskPaper scripts/small_time_004.py
@@ -60,8 +60,10 @@ def main():
 #    print tag_updates(TXT_SAMPLE2)
 
     print "Text with translations and date deferrals automatically applied:\n"
-    print defer_dates(['start', 'due'], '+4d', TXT_SAMPLE), '\n'
-    print defer_dates(['start'], '-1w', TXT_SAMPLE2)
+    print "FROM (start & due +4d):", TXT_SAMPLE
+    print "TO: ", defer_dates(['start', 'due'], '+4d', TXT_SAMPLE), '\n'
+    print "FROM (start only -1w):", TXT_SAMPLE2
+    print "TO: ", defer_dates(['start'], '-1w', TXT_SAMPLE2)
     print '\n', examples()
 
 def examples():
