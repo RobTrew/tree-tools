117d08819e007cb0a0d4ffde91d53772451cdee0
Rob Trew <houthakker72@gmail.com>
2014-04-10 00:41:21 +0100

    Added FoldingText and/or TaskPaper plugin
    Two key-strokes: ‘Collapse more’ and ‘Expand more’ – decreasing or
    increasing the expansion level of the outline in steps.
    Cmd Alt - and Cmd Alt +

---
+++ FoldingText 2 plugins and scripts/expand more or less.ftplugin/main.js
@@ -0,0 +1,97 @@
+define(function(require, exports, module) {
+	'use strict';
+
+	var Extensions = require('ft/core/extensions');
+
+	// deepest nesting level in this sub-tree ?
+	function maxdepth(node) {
+		var lngChiln = 0, lstChiln=[],
+			lngMax = 0, lngDepth = 0, i = 0;
+
+		if (node.hasChildren()) {
+			lstChiln = node.children();
+			lngChiln = lstChiln.length;
+			for (i = 0; i < lngChiln; i++) {
+				lngDepth = maxdepth(lstChiln[i]) + 1;
+				if (lngDepth > lngMax) {
+					lngMax = lngDepth;
+				}
+			}
+		}
+		return lngMax;
+	}
+
+	// shallowest fold in the editor's tree ?
+	function getFoldLevel(editor) {
+		function nestLevel(node) {
+			var level = 0;
+			while (node.parent) {
+				level++;
+				node = node.parent;
+			}
+			return level;
+		}
+		// what folds have we got ?
+		var tree = editor.tree(), folds = editor.folds(),
+			lstLevels = [], i=0, lngFolds = folds.length, lngLine;
+
+		// and how deep is the shallowest fold ?
+		for (i=0; i < lngFolds; i++) {
+			lngLine = folds[i].range().startLine();
+			lstLevels.push(nestLevel(tree.lineNumberToNode(lngLine)));
+		}
+		// null if no folds
+		return lstLevels.sort()[0];
+	}
+
+	function setFoldLevel(editor, lngLevel) {
+		// clear all existing folds
+		// and fold all nodes at level N
+		var strPath = new Array(lngLevel+1).join('/*'),
+			lstNodes = editor.tree().evaluateNodePath(strPath);
+		editor.removeFolds(editor.folds());
+		editor.collapseNodes(lstNodes, false);
+	}
+
+
+	Extensions.add('com.foldingtext.editor.commands', {
+		name: 'expand more',
+		description: 'Expand outline one more level',
+		performCommand: function (editor) {
+			var lngMaxDepth = maxdepth(editor.tree().root),
+				lngLevel = getFoldLevel(editor);
+
+			if (lngLevel) { // not null
+				if (lngLevel < (lngMaxDepth)) {
+					setFoldLevel(editor, lngLevel + 1);
+				}
+			}
+		}
+
+	});
+
+
+	Extensions.add('com.foldingtext.editor.commands', {
+		name: 'expand less',
+		description: 'Collapse outline one more level',
+		performCommand: function (editor) {
+			var lngLevel = getFoldLevel(editor);
+			if (lngLevel) {
+				if (lngLevel > 1) {
+					setFoldLevel(editor, lngLevel - 1);
+				}
+			} else { // folder the outer fringe
+				setFoldLevel(editor, maxdepth(editor.tree().root) - 1);
+			}
+		}
+	});
+
+	Extensions.add('com.foldingtext.editor.init', function (editor) {
+		editor.addKeyMap({
+			'Cmd-Alt-=' : 'expand more',
+			'Cmd-Alt--' : 'expand less',
+		});
+	});
+
+
+});
---
+++ FoldingText 2 plugins and scripts/expand more or less.ftplugin/spec.js
@@ -0,0 +1,59 @@
+define(function (require) {
+	'use strict';
+
+	describe('Expand and collapse', function () {
+		var MarkdownTaxonomy = require('ft/taxonomy/markdowntaxonomy').MarkdownTaxonomy,
+			Taxonomies = require('ft/core/taxonomies'),
+			Editor = require('ft/editor/editor').Editor,
+			taxonomy = Taxonomies.taxonomy({
+				foldingtext: true,
+				multimarkdown: true,
+				gitmarkdown: true,
+				criticMarkup: true
+			}, 'markdown'),
+			editor;
+
+		beforeEach(function () {
+			editor = new Editor('', taxonomy);
+		});
+
+		afterEach(function () {
+			editor.removeAndCleanupForCollection();
+		});
+
+		it('level of folding should increase in steps', function () {
+			var lstFolds = [], i=0;
+			editor.setTextContent('# a\n- b\n\t- c\n\t\t- d\n# e\n- f\n\t- g\n\t\t- h');
+			editor.performCommand('expand less');
+			editor.performCommand('expand less');
+			for (i = 0; i < 2; i++) {
+				lstFolds.push(editor.folds()[i].range().startLine());
+			}
+			expect(editor.folds().length).toEqual(2);
+			expect(lstFolds[0]).toEqual(1);
+			expect(lstFolds[1]).toEqual(5);
+		});
+
+		it('level of folding should decrease in steps', function () {
+			var lstFolds = [], i=0;
+			editor.setTextContent('# a\n- b\n\t- c\n\t\t- d\n# e\n- f\n\t- g\n\t\t- h');
+			editor.performCommand('expand less');
+			editor.performCommand('expand less');
+			editor.performCommand('expand less');
+			editor.performCommand('expand more');
+			editor.performCommand('expand more');
+			for (i = 0; i < 2; i++) {
+				lstFolds.push(editor.folds()[i].range().startLine());
+			}
+			expect(editor.folds().length).toEqual(2);
+			expect(lstFolds[0]).toEqual(2);
+			expect(lstFolds[1]).toEqual(6);
+		});
+
+
+	});
+
+
+});
+
+
