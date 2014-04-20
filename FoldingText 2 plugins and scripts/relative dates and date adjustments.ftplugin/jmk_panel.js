b3a969034b479d53cb81b1f8577c9666a8c71663
Rob Trew <houthakker72@gmail.com>
2014-04-20 22:05:38 +0100

    Updated version of jmk_panel

---
+++ FoldingText 2 plugins and scripts/relative dates and date adjustments.ftplugin/jmk_panel.js
@@ -0,0 +1,258 @@
+/* ------------------------------------------------------------- *
+ * Panel Module for FoldingText 2.0 Plugins
+ * by Jamie Kowalski, github.com/jamiekowalski/foldingtext-extra
+ * Use of this code and associated files is permitted without restriction, provided
+ * the above attribution and this statement are included in all copies of this code.
+ *
+ * Usage:
+ *
+ * The event callback functions are passed the event object and the panel object;
+ * but there is no need to capture these in your functions if they are not needed.
+ *
+ * None of the options are required; define as many functions as you need.
+ *
+ * Return false from a function to prevent the default behavior (e.g. closing panel
+ * on return).
+ *
+ * var Panel = require('./panel.js').Panel;
+ *
+ * var panel = new Panel({
+ *   className: 'MyPanel',
+ *   placeholder: 'type some text...',
+ *   onTextChange: function (event, panel) {},
+ *   onBlur: function (event, panel) {},
+ *   onReturn: function (event, panel) {},
+ *   onEscape: function (event, panel) {},
+ *   onCommand: function (event, panel) {}, // use event.which to determine which
+ *                                          // key other than Command is pressed
+ *   ignoreWhiteSpace: true,     // don't get changes in leading/trailing whitespace
+ *   addToDOM: true
+ * });
+ *
+ * ------------------------------------------------------------- */
+
+define(function(require, exports, module) {
+	'use strict';
+
+	var Extensions = require('ft/core/extensions'),
+		editor;          // this variable is assigned in the 'init' function below
+
+	// Panel constructor; assigned to exports.Panel below
+	var p = function (opts) {
+
+		var commonClassName = 'JKPanel',
+			COMMAND_LEFT = 91,
+			COMMAND_RIGHT = 93,
+			RETURN = 13,
+			ESC = 27,
+			KEY_A = 65,
+			KEY_Z = 90,
+			debug = false,
+			no_op = function () {};    // no-op function to use as default
+
+		// define default options
+		this.options = {
+			className: '',
+			placeholder: 'enter text...',
+			onTextChange: no_op,
+			onBlur: no_op,
+			onReturn: no_op,
+			onEscape: no_op,
+			onCommand: no_op,
+			ignoreWhiteSpace: true,
+			addToDOM: true
+		};
+
+		// copy options from argument
+		for (var op in opts) {
+
+			if ( this.options[op] === undefined ) {
+				console.log('JKPanel: \'' + op + '\' is not a valid option. Ignoring.');
+			} else if ( typeof this.options[op] !== typeof opts[op] ) {
+				console.log( 'JKPanel: Option \'' + op + '\' must be of type ' +
+					typeof this.options[op] + '. Reverting to default.' );
+			} else {
+				this.options[op] = opts[op];
+			}
+		}
+
+		this.isShown = false;    // TODO hide this property
+		this.currentValue = '';
+
+		// add unsettable properties
+		Object.defineProperty(
+			this,
+			'element',
+			{ value: document.createElement('div') }
+		);
+		Object.defineProperty(
+			this,
+			'input',
+			{ value: document.createElement('input') }
+		);
+
+		// set panel attributes
+		this.element.style.display = 'none';       // don't show panel at first
+		this.element.className = commonClassName;
+
+		this.input.setAttribute('type', 'text');
+		this.input.setAttribute('value', '');
+		this.input.setAttribute('placeholder', this.options.placeholder);
+		if (this.options.className) {
+			this.element.classList.add(this.options.className);
+		}
+
+		this.element.insertBefore(this.input); // add the input to the panel
+
+		// when editor is clicked, etc.
+		this.input.addEventListener('blur', (function(event) {
+			var performDefault;
+			if (this.options.onBlur && this.options.onBlur !== no_op) {
+				performDefault = this.options.onBlur(event, this);
+			}
+			if (performDefault !== false) {     // panel's default behavior
+				this.hide(true);    // close the panel, keep contents
+			}
+		}).bind(this));
+
+		// capture changes to input
+		this.input.addEventListener('input', (function(event) {
+			if (debug) console.log( 'input change event: \'' + this.input.value + '\'' );
+
+			if (this.options.ignoreWhiteSpace) {
+				if ( this.input.value.trim() === this.currentValue ) {
+					if (debug) console.log( 'No change' );
+
+				} else {
+					if (debug) console.log( 'Text changed' );
+					this.currentValue = this.input.value.trim();
+
+					if (this.options.onTextChange && this.options.onTextChange !== no_op) {
+						this.options.onTextChange(event, this);
+					}
+				}
+			} else {
+				if (this.options.onTextChange && this.options.onTextChange !== no_op) {
+					this.options.onTextChange(event, this);
+				}
+			}
+
+		}).bind(this));
+
+		// capture keyups (for command keys, etc.)
+		this.input.addEventListener('keyup', (function(event) {
+			if (debug) console.log('keyup: ' + event.which);
+			if (debug) console.log(this.keysDown);
+
+			if (event.which === COMMAND_LEFT) {          // left command key
+				this.keysDown[COMMAND_LEFT] = false;
+			} else if (event.which === COMMAND_RIGHT) {  // right command key
+				this.keysDown[COMMAND_RIGHT] = false;
+			}
+
+		}).bind(this));
+
+		// capture keydowns (for command keys, etc.)
+		this.input.addEventListener('keydown', (function(event) {
+			var performDefault;
+			if (debug) console.log('keydown: ' + event.which);
+
+			if (event.which === RETURN) {                  // return key pressed
+
+				if (this.options.onReturn && this.options.onReturn !== no_op ) {
+					performDefault = this.options.onReturn(event, this);
+				}
+				if (performDefault !== false) {      // panel's default behavior
+					this.input.dispatchEvent(new CustomEvent('blur'));
+					event.preventDefault();
+				}
+
+			} else if ( event.which === COMMAND_LEFT ) {     // command keys pressed
+				this.keysDown[COMMAND_LEFT] = true;
+			} else if ( event.which === COMMAND_RIGHT ) {
+				this.keysDown[COMMAND_RIGHT] = true;
+			} else if ( event.which === ESC ) {            // escape key pressed
+
+				if ( this.options.onEscape && this.options.onEscape !== no_op ) {
+					this.options.onEscape( event, this );
+				}
+				if ( performDefault !== false ) {        // panel's default behavior
+					this.hide(false);
+					event.preventDefault();
+				}
+
+			} else if ( this.keysDown[COMMAND_LEFT] || this.keysDown[COMMAND_RIGHT] ) {
+				// Modify behavior of some command combinations
+
+				if ( this.options.onCommand && this.options.onCommand !== no_op ) {
+					performDefault = this.options.onCommand( event, this );
+				}
+				if (performDefault !== false ) {
+					if ( event.which === KEY_A ) {           // Command + A
+						this.input.select();
+						event.preventDefault();
+					} else if ( event.which === KEY_Z ) {    // Command + Z
+						event.preventDefault();
+					}
+				}
+
+			}
+		}).bind(this));
+
+		if ( this.options.addToDOM ) {
+			document.body.insertBefore( this.element );
+		}
+	};
+
+	Object.defineProperty( p.prototype, 'keysDown', { value: {} } );
+
+	p.prototype.addToDOM = function () {
+		document.body.insertBefore( this.element );
+	};
+	p.prototype.show = function ( text ) {
+		if ( text ) {
+			this.input.value = text;
+		}
+		this.element.style.display = 'block';
+		this.input.select();          // select contents, and focus input
+		if ( this.options.ignoreWhiteSpace ) {
+			this.currentValue = this.input.value.trim();
+		}
+		this.isShown = true;
+	};
+	p.prototype.hide = function ( keepContents ) {
+		if ( !keepContents ) {
+			this.input.value = '';
+		}
+		if ( this.options.ignoreWhiteSpace ) {
+			this.currentValue = this.input.value.trim();
+		}
+		this.element.style.display = 'none';
+		editor.focus();
+		this.isShown = false;
+	};
+	p.prototype.toggle = function ( keepContents, text ) {
+		if ( this.isShown ) {
+			this.hide( keepContents );
+		} else {
+			this.show( text );
+		}
+	};
+	p.prototype.clear = function () {
+		this.input.value = '';
+	};
+	p.prototype.value = function () {
+		if ( this.options.ignoreWhiteSpace ) {
+			return this.input.value.trim();
+		} else {
+			return this.input.value;
+		}
+	};
+
+	Extensions.add('com.foldingtext.editor.init', function( ed ) {
+		editor = ed;
+	});
+
+	exports.Panel = p;
+
+});
--- FoldingText 2 plugins and scripts/relative dates and date adjustments.ftplugin/main.js
+++ FoldingText 2 plugins and scripts/relative dates and date adjustments.ftplugin/main.js
@@ -1,25 +1,13 @@
-// Date translations by Robin Trew 2014, MIT License
-//
-// Popup panel by Jamie Kowalski
-//
-/* ------------------------------------------------------------- *
- * Panel Module for FoldingText 2.0 Plugins
- * by Jamie Kowalski, github.com/jamiekowalski/foldingtext-extra
- *
- * Usage:
- *
- * var Panel = require('./panel.js').Panel;
- *
- * var panel = new Panel({
- *   className: String,
- *   placeholder: String,
- *   onReturn: Function,
- *   onEscape: Function,
- *   onTextChange: Function,
- *   addToDOM: Boolean    // add to DOM immediately? Default true
- * })
- *
- * ------------------------------------------------------------- */
+// Plugin for TaskPaper 3 and FoldingText 2
+// Live translation of informal and relative date entry
+// Place cursor in or next to date tag, and run plugin
+// to enter or adjust an absolute ISO date/time using
+// informal relative phrases like today + 7d, jul 14, next month etc
+// Author Robin Trew 2014, MIT License
+// https://github.com/RobTrew/tree-tools
+
+// README.md
+// https://github.com/RobTrew/tree-tools/blob/master/FoldingText%202%20plugins%20and%20scripts/relative%20dates%20and%20date%20adjustments.ftplugin/README.md
 
 
 define(function(require, exports, module) {
@@ -27,7 +15,7 @@ define(function(require, exports, module) {
 
 	var Extensions = require('ft/core/extensions'),
 		dateLogic = require('./smalltime.js'),
-		Panel = require('./jmkpanel.js').Panel,
+		Panel = require('./jmk_panel.js').Panel,
 		panel,
 		Editor;
 
@@ -129,14 +117,11 @@ define(function(require, exports, module) {
 			className: 'RTDatePanel',
 			placeholder: 'enter date phrase ...',
 			onReturn: function() {
-				translatePhrase(panel.input.value);
-				panel.input.value = '';
+				panel.clear();
 				Editor.performCommand('moveRight');
-
 			},
-			onEscape: function() {
-				panel.hide();
-				panel.input.value = '';
+			onBlur: function() {
+				panel.clear();
 			},
 			onTextChange: function() {
 				translatePhrase(panel.input.value);
@@ -147,27 +132,6 @@ define(function(require, exports, module) {
 			"Cmd-Alt-'" : show_date_panel
 			//Shift-, Cmd-, Ctrl-, and Alt- (in that order!) to
 		});
-	});
-
-// List date phrases and their ISO translations to the console
-// window.peformAction('test date phrases')
-	Extensions.add('com.foldingtext.editor.commands', {
-		name: 'test date phrases',
-		description: 'translate date phrase to ISO 8601',
-		performCommand: function (editor) {
-			var lstIn = ['last month +3h','+1', '-1','w', 'd', 'y', '16h', '1m', '16H', '1M', 'now + 1h', 'now + 1m', 'o', 'next month', 'last month', '2021 aug 10', '2021 10 aug ', 'aug 10 2021', '10 aug 2021', '21:15','aug', 'jan', '6am january 3', '3 jan 6am', 'today +7d', '11:20 +7d', 'today', 'today -7d', '11:20 -7d', 'last thursday', 'next thursday', 'last week', 'next week', 'in 18 months', 'in 2 months', 'yesterday 8am', 'yesterday 8:00','+2d +3d', 'aug 10 2019', '+3y', 'jan 10 -3d', 'tomorrow 2pm', 'aug 10 2019', 'aug 2019', 'today -7d', '11:20 +4d', '2014-02-15 +1w', 'jan 10', 'jan 10 2pm', 'jan 10 11am', '10 jan at 10am', 'now-3d', '+7d', '7', '11:15', '11:15p', '11p', 'jan 5', 'now', 'now +4h', 'today + 1w', '1w', '1w', '8am monday', 'monday 8am', '8am tomorrow', 'wed at 10', 'friday 2pm', 'monday', 'next monday', 'tuesday', 'next tuesday', 'wednesday', 'next wednesday', 'thursday', 'next thursday', 'friday', 'next friday', 'saturday', 'next saturday', 'sunday', 'next sunday'],
-				lenTest = lstIn.length, i, dctResult=null,
-				strPhrase='', strTrans='', strOut='';
-
-			for (i=0; i<lenTest; i++) {
-				strPhrase = lstIn[i];
-
-
-				strTrans = dateLogic.datePhraseToISO(strPhrase);
-				strOut += ['[' + strPhrase + ']→', strTrans].join('\t') + '\n';
-			}
-			console.log(strOut);
-		}
 	});
 
 });
