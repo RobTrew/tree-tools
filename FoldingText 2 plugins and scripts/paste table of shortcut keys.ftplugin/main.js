52ae84b22f5dc1739cbc9f272b64127bdc33259d
Rob Trew <houthakker72@gmail.com>
2014-04-27 15:43:54 +0100

    Added a plugin for pasting a table of FT shortcuts
    Shift-Cmd-Ctrl-Alt-K
    pastes shortcut table into current FT document

---
+++ FoldingText 2 plugins and scripts/paste table of shortcut keys.ftplugin/main.js
@@ -0,0 +1,71 @@
+// Paste a full map of FT keyboard assignments as an MMD table
+// Shift-Cmd-Ctrl-Alt-K
+
+define(function(require, exports, module) {
+	'use strict';
+
+	var Extensions = require('ft/core/extensions'),
+		pasteboard = require('ft/system/pasteboard'),
+		dctShowKeys = {'name':'show map', 'keys':'Shift-Cmd-Ctrl-Alt-K'},
+		dctKeys = {};
+
+	function pasteKeyMap(editor) {
+		var lstKeys = sortedKeys(editor),
+			strHead = '| Key | Assignment |\n|:----|:-----------|\n',
+			strKeys = strHead + lstKeys.join('\n');
+
+			editor.replaceSelection('Please wait a moment ...', 'around');
+			editor.replaceSelection(strKeys);
+			pasteboard.writeString(strKeys);
+	}
+
+	function sortedKeys(editor) {
+		var lstMaps = editor._keyMaps,
+			lngMaps = lstMaps.length, i,
+			dctMap = {}, strKey, varVal, lstKeys=[];
+
+		for (i=0; i< lngMaps; i++) {
+			dctMap = lstMaps[i];
+			for (strKey in dctMap) {
+				if (strKey !== 'name') {
+					varVal = dctMap[strKey];
+					if (!(varVal && typeof(varVal) === 'string')) {
+						varVal = typeof(varVal);
+					}
+					lstKeys.push('| `' + keySymbols(strKey) +
+						'` | ' + varVal.split('\n')[0] + ' |');
+				}
+			}
+		}
+		lstKeys.sort();
+		return lstKeys;
+	}
+
+	function keySymbols(strKeys) {
+		return strKeys.replace(
+			'Shift-', '⇧').replace(
+			'Cmd-', '⌘').replace(
+			'Ctrl-',  '^').replace(
+			'Alt-', '⌥');
+	}
+
+	Extensions.add('com.foldingtext.editor.commands', {
+		name: dctShowKeys.name,
+		description: 'show keyboard assignments',// + ' - ' + keySymbols(
+			//dctDeep.keys),
+		performCommand: function (editor, pasteboard) {
+
+			pasteKeyMap(editor);
+		}
+	});
+
+	Extensions.add('com.foldingtext.editor.init', function (editor) {
+		dctKeys[dctShowKeys.keys] = dctShowKeys.name;
+		editor.addKeyMap(dctKeys);
+	});
+
+
+});
+
+
+//
