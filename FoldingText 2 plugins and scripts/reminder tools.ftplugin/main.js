9dc84e20ac7cb851b1bc0d48bdbac462eda448da
Rob Trew <houthakker72@gmail.com>
2014-05-05 00:54:05 +0100

    FT/TPUpdateReminder v. 010
    (Use updated smalltime and reminder tools plugins)

--- FoldingText 2 plugins and scripts/reminder tools.ftplugin/main.js
+++ FoldingText 2 plugins and scripts/reminder tools.ftplugin/main.js
@@ -5,29 +5,59 @@
 //		through window.RTpluginReturn, so that a linked Reminder.app
 //		can be updated or created.
 
-
 define(function(require, exports, module) {
 	'use strict';
 
 	var Extensions = require('ft/core/extensions'),
-		dateLogic = require('../smalltime.ftplugin/smalltime.js'),
+		dateLogic = require('../smalltime.ftplugin/main.js'),
 		Editor;
+
+	function updateAndReadForLink(dctArgs) {
+
+		var	lstResults = translateDateTags(Editor, dctArgs),
+			node = lstResults[0], strLine = node.line(),
+			strText = node.text(),
+			dctReturn = lstResults[1], strTag='', oMatch,
+			lstHeat = dctArgs.heat || [], lngHeat = 0, i,
+			rgxReminder = /\[[^\]]*\]\((x-apple-reminder:\/\/[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12})\)/;
+
+		// Find the highest priority/heat tag and return a 1-based index
+		lngHeat = lstHeat.length;
+		for (i=0; i<lngHeat; i++) {
+			strTag = lstHeat[i];
+			if (strTag.charAt(0) !== '@') {strTag = ('@' + strTag);}
+			if (strLine.indexOf(strTag) !== -1) {
+				if (i > 2) {i=2;}
+				dctReturn.heat = i+1;
+				break;
+			}
+		}
+
+		// Separate any Reminders UUID from the main text entry
+		oMatch = rgxReminder.exec(strText);
+		if (oMatch) {
+			dctReturn.uuid = oMatch[1];
+			dctReturn.text = strText.substring(0, oMatch.index-1);
+		} else {
+			dctReturn.text = strText;
+		}
+		// return the harvest for any calling Applescript
+		return dctReturn;
+	}
 
 	// translate values of specified date tags to ISO (if informal/relative)
 	// and return any values for uuid, text, alarm tag, priority,
 	// and any other requested date tags
-	function translatedDateTags(Editor, dctArgs) {
+	function translateDateTags(Editor, dctArgs) {
 		var lstUpdateTags = ['alarm'],
 			tree = Editor.tree(),
 			node = Editor.selectedRange().startNode,
 			dctReturn = {'uuid':null, 'text':null, 'alarm':null,
 				'datetext':null, 'iso':null, 'heat':null},
-			dctNodeTags = node.tags(), strLine = node.line(),
+			dctNodeTags = node.tags(),
 			lstNodeTags = Object.keys(dctNodeTags), strKey='', strVal,
-			lngSeconds=0, lstKeySet, strTag, lngTag,
-			strTrans='', dteUpdated, strAlarmKey, blnDelta,
-			rgxReminder = /\[[^\]]*\]\((x-apple-reminder:\/\/[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12})\)/, oMatch,
-			lstHeat=[], lngHeat = 0, i;
+			lngSeconds=0, lstKeySet, lngTag,
+			strTrans='', dteUpdated, strAlarmKey, blnDelta, i;
 
 		// read any arguments to override defaults for alarm,
 		// other date tags, and priority tags
@@ -38,7 +68,6 @@ define(function(require, exports, module) {
 			}
 			lstUpdateTags = dctArgs.others || [];
 			lstUpdateTags.push(strAlarmKey);
-			lstHeat = dctArgs.heat || [];
 		}
 		// strip any leading '@' from date tags to get the key
 		lngTag = lstUpdateTags.length;
@@ -80,42 +109,25 @@ define(function(require, exports, module) {
 		});
 		tree.endUpdates();
 
-		// Find the highest priority/heat tag and return a 1-based index
-		lngHeat = lstHeat.length;
-		for (i=0; i<lngHeat; i++) {
-			strTag = lstHeat[i];
-			if (strTag.charAt(0) !== '@') {strTag = ('@' + strTag);}
-			if (strLine.indexOf(strTag) !== -1) {
-				dctReturn.heat = i+1;
-				break;
-			}
-		}
-
-		// Separate any Reminders UUID from the main text entry
-		oMatch = rgxReminder.exec(node.line());
-		if (oMatch) {
-			dctReturn.uuid = oMatch[1];
-			dctReturn.text = node.text().substring(0, oMatch.index-2);
-		} else {
-			dctReturn.text = node.text();
-		}
-		// Use a global to return the harvest to any calling Applescript
-		window.RTpluginReturn = dctReturn;
+		return [node, dctReturn];
 	}
 
 
+	//translate date tag(s) to ISO and return UUID, date(s), priorty, text
+	exports.updateAndReadForLink = updateAndReadForLink;
+	exports.version = 2.0;
+
 	Extensions.add('com.foldingtext.editor.commands', {
-		name: 'interpret date tags',
-		description: 'translate to ISO absolute',
-		performCommand: translatedDateTags
+		name: 'translate date tags',
+		description: 'convert to ISO absolute',
+		performCommand: translateDateTags
 	});
 
 	Extensions.add('com.foldingtext.editor.init', function(editor) {
 		Editor = editor;
 
 		Editor.addKeyMap({
-			//open bracket with Cmd held down
-			"Shift-Cmd-U" : 'interpret date tags'
+			"Shift-Cmd-U" : 'translate date tags'
 		});
 	});
 
